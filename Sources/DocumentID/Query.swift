0daf4f0e5acb4ab205493f71f2c963db82d96102
1amageek <tmy0x3@icloud.com>
2022-05-21 02:01:50 +0900

    fix encode

--- Sources/DocumentID/Query.swift
+++ Sources/DocumentID/Query.swift
@@ -327,21 +327,65 @@ extension QueryPredicate: Encodable {
 extension KeyedEncodingContainer where Key : CodingKey {
 
     fileprivate mutating func encode(_ value: Any, forKey key: KeyedEncodingContainer<K>.Key) throws {
-        if let value = value as? String {
+        if let value = value as? Bool {
+            try encode(value, forKey: key)
+        } else if let value = value as? String {
+            try encode(value, forKey: key)
+        } else if let value = value as? Double {
+            try encode(value, forKey: key)
+        } else if let value = value as? Float {
             try encode(value, forKey: key)
         } else if let value = value as? Int {
             try encode(value, forKey: key)
-        } else if let value = value as? Double {
+        } else if let value = value as? Int8 {
+            try encode(value, forKey: key)
+        } else if let value = value as? Int16 {
+            try encode(value, forKey: key)
+        } else if let value = value as? Int32 {
+            try encode(value, forKey: key)
+        } else if let value = value as? Int64 {
+            try encode(value, forKey: key)
+        } else if let value = value as? UInt {
+            try encode(value, forKey: key)
+        } else if let value = value as? UInt8 {
+            try encode(value, forKey: key)
+        } else if let value = value as? UInt16 {
+            try encode(value, forKey: key)
+        } else if let value = value as? UInt32 {
+            try encode(value, forKey: key)
+        } else if let value = value as? UInt64 {
             try encode(value, forKey: key)
         }
     }
 
     fileprivate mutating func encode(_ value: [Any], forKey key: KeyedEncodingContainer<K>.Key) throws {
-        if let value = value as? [String] {
+        if let value = value as? [Bool] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [String] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [Double] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [Float] {
             try encode(value, forKey: key)
         } else if let value = value as? [Int] {
             try encode(value, forKey: key)
-        } else if let value = value as? [Double] {
+        } else if let value = value as? [Int8] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [Int16] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [Int32] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [Int64] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [UInt] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [UInt8] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [UInt16] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [UInt32] {
+            try encode(value, forKey: key)
+        } else if let value = value as? [UInt64] {
             try encode(value, forKey: key)
         }
     }
