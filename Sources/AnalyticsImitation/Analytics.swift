9c08111b68de31fdf80dd501ccdf3f25b2d618d5
1amageek <tmy0x3@icloud.com>
2022-05-21 17:37:01 +0900

    Fix

--- Sources/AnalyticsImitation/Analytics.swift
+++ Sources/AnalyticsImitation/Analytics.swift
@@ -9,18 +9,20 @@ import Foundation
 
 public protocol Analytics {
 
+    static func logEvent(_ name: String, parameters: [String: Any]?)
+    static func setUserProperty(_ value: String?, forName name: String)
     static func setUserID(_ userID: String?)
-    static func setUserProperty(_ value: String?, for name: String)
-    static func logEvent(name: String, parameters: [String: Any]?)
+    static func setAnalyticsCollectionEnabled(_ analyticsCollectionEnabled: Bool)
     static func setSessionTimeoutInterval(_ sessionTimeoutInterval: TimeInterval)
+    static func appInstanceID() -> String?
     static func resetAnalyticsData()
     static func setDefaultEventParameters(_ parameters: [String: Any]?)
 }
 
 extension Analytics {
 
-    static func logEvent(name: String) {
-        logEvent(name: name, parameters: nil)
+    static func logEvent(_ name: String) {
+        logEvent(name, parameters: nil)
     }
 
     static func setDefaultEventParameters() {
