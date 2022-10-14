3f815c0933df82ae68d775432361d7b816914804
1amageek <nori@stamp.team>
2022-10-14 16:43:13 +0900

    Fix

--- Sources/StorageImitation/Storage.swift
+++ Sources/StorageImitation/Storage.swift
@@ -9,13 +9,13 @@ import Foundation
 
 public protocol Storage {
     func putData(ref: StorageReference, data: Data, metadata: StorageMetadata) async throws
-    func getData(ref: StorageReference, maxSize: Int64) async throws -> Data?
+    func getData(ref: StorageReference, maxSize: Int64) async throws -> Data
     func delete(ref: StorageReference) async throws
 }
 
 extension Storage {
     
-    public func getData(ref: StorageReference) async throws -> Data? {
-        try await getData(ref: ref, maxSize: 3 * 1024 * 1024)        
+    public func getData(ref: StorageReference) async throws -> Data {
+        try await getData(ref: ref, maxSize: 3 * 1024 * 1024)
     }
 }
