e004a32bf94cf61c882f4255780cbf3fd10ac98a
1amageek <nori@stamp.team>
2023-05-10 21:29:11 +0900

    Fix

--- Sources/FunctionsImitation/Functions.swift
+++ Sources/FunctionsImitation/Functions.swift
@@ -13,3 +13,28 @@ public protocol Functions {
 
     func request<T: Decodable>(url: URL, type: T.Type, decoder: JSONDecoder) async throws -> T?
 }
+
+extension Functions {
+
+    public static func iso8601() -> JSONDecoder {
+        let decoder = JSONDecoder()
+        let dateFormatter = DateFormatter()
+        dateFormatter.calendar = Calendar(identifier: .gregorian)
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return decoder
+    }
+
+    public static func rfc822() -> JSONDecoder {
+        let decoder = JSONDecoder()
+        let dateFormatter = DateFormatter()
+        dateFormatter.calendar = Calendar(identifier: .gregorian)
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return decoder
+    }
+}
