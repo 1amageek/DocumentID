340c1fac967b81d86f32a494b65fc86b1d0c1a4c
1amageek <nori@stamp.team>
2024-09-23 13:27:23 +0900

    Update Firestore.swift to support sending and receiving Encodable and Sendable types

--- Sources/FirestoreImitation/Firestore.swift
+++ Sources/FirestoreImitation/Firestore.swift
@@ -16,20 +16,20 @@ public enum Source: Sendable {
 
 public protocol Firestore: Sendable {
     func updates(_ reference: DocumentReference, includeMetadataChanges: Bool) -> AsyncThrowingStream<DocumentSnapshot?, Error>?
-    func updates<T: Decodable>(_ reference: DocumentReference, includeMetadataChanges: Bool, type: T.Type) -> AsyncThrowingStream<T?, Error>?
-    func updates<T: Decodable>(_ query: Query, includeMetadataChanges: Bool, type: T.Type) -> AsyncThrowingStream<[T], Error>?
-    func changes<T: Decodable>(_ query: Query, includeMetadataChanges: Bool, type: T.Type) -> AsyncThrowingStream<(added: [T], modified: [T], removed: [T]), Error>?
+    func updates<T: Decodable & Sendable>(_ reference: DocumentReference, includeMetadataChanges: Bool, type: T.Type) -> AsyncThrowingStream<T?, Error>?
+    func updates<T: Decodable & Sendable>(_ query: Query, includeMetadataChanges: Bool, type: T.Type) -> AsyncThrowingStream<[T], Error>?
+    func changes<T: Decodable & Sendable>(_ query: Query, includeMetadataChanges: Bool, type: T.Type) -> AsyncThrowingStream<(added: [T], modified: [T], removed: [T]), Error>?
     
     func get(_ reference: DocumentReference, source: Source) async throws -> DocumentSnapshot?
-    func get<T: Decodable>(_ query: Query, source: Source, type: T.Type) async throws -> [T]?
-    func get<T: Decodable>(_ reference: DocumentReference, source: Source, type: T.Type) async throws -> T?
+    func get<T: Decodable & Sendable>(_ query: Query, source: Source, type: T.Type) async throws -> [T]?
+    func get<T: Decodable & Sendable>(_ reference: DocumentReference, source: Source, type: T.Type) async throws -> T?
     func get(_ aggregateQuery: AggregateQuery) async throws -> AggregateQuerySnapshot?
     func set(_ data: [String: Any], merge: Bool, reference: DocumentReference) async throws
-    func set<T: Encodable>(_ data: T, merge: Bool, reference: DocumentReference) async throws
-    func set<T: Encodable>(_ data: T, extensionData: [String: Any], merge: Bool, reference: DocumentReference) async throws
+    func set<T: Encodable & Sendable>(_ data: T, merge: Bool, reference: DocumentReference) async throws
+    func set<T: Encodable & Sendable>(_ data: T, extensionData: [String: Any], merge: Bool, reference: DocumentReference) async throws
     func update(_ data: [String: Any], reference: DocumentReference) async throws
-    func update<T: Encodable>(_ data: T, reference: DocumentReference) async throws
-    func update<T: Encodable>(_ data: T, extensionData: [String: Any], reference: DocumentReference) async throws
+    func update<T: Encodable & Sendable>(_ data: T, reference: DocumentReference) async throws
+    func update<T: Encodable & Sendable>(_ data: T, extensionData: [String: Any], reference: DocumentReference) async throws
     func delete(reference: DocumentReference) async throws
     func writeBatch() -> WriteBatch
     func runTransaction(update: @escaping (Transaction, NSErrorPointer) -> Any?, completion: @escaping (Any?, Error?) -> Void)
@@ -41,31 +41,31 @@ extension Firestore {
         updates(reference, includeMetadataChanges: includeMetadataChanges)
     }
     
-    public func updates<T: Decodable>(_ reference: DocumentReference, includeMetadataChanges: Bool = true, type: T.Type) -> AsyncThrowingStream<T?, Error>? {
+    public func updates<T: Decodable & Sendable>(_ reference: DocumentReference, includeMetadataChanges: Bool = true, type: T.Type) -> AsyncThrowingStream<T?, Error>? {
         updates(reference, includeMetadataChanges: includeMetadataChanges, type: type)
     }
 
-    public func updates<T: Decodable>(_ reference: CollectionReference, includeMetadataChanges: Bool = true, type: T.Type) -> AsyncThrowingStream<[T], Error>? {
+    public func updates<T: Decodable & Sendable>(_ reference: CollectionReference, includeMetadataChanges: Bool = true, type: T.Type) -> AsyncThrowingStream<[T], Error>? {
         updates(Query(reference.path), includeMetadataChanges: includeMetadataChanges, type: type)
     }
     
-    public func updates<T: Decodable>(_ query: Query, includeMetadataChanges: Bool = true, type: T.Type) -> AsyncThrowingStream<[T], Error>? {
+    public func updates<T: Decodable & Sendable>(_ query: Query, includeMetadataChanges: Bool = true, type: T.Type) -> AsyncThrowingStream<[T], Error>? {
         updates(query, includeMetadataChanges: includeMetadataChanges, type: type)
     }
 
-    public func changes<T: Decodable>(_ reference: CollectionReference, includeMetadataChanges: Bool = true, type: T.Type) -> AsyncThrowingStream<(added: [T], modified: [T], removed: [T]), Error>? {
+    public func changes<T: Decodable & Sendable>(_ reference: CollectionReference, includeMetadataChanges: Bool = true, type: T.Type) -> AsyncThrowingStream<(added: [T], modified: [T], removed: [T]), Error>? {
         changes(Query(reference.path), includeMetadataChanges: includeMetadataChanges, type: type)
     }
     
-    public func changes<T: Decodable>(_ query: Query, includeMetadataChanges: Bool = true, type: T.Type) -> AsyncThrowingStream<(added: [T], modified: [T], removed: [T]), Error>? {
+    public func changes<T: Decodable & Sendable>(_ query: Query, includeMetadataChanges: Bool = true, type: T.Type) -> AsyncThrowingStream<(added: [T], modified: [T], removed: [T]), Error>? {
         changes(query, includeMetadataChanges: includeMetadataChanges, type: type)
     }
 
-    public func get<T: Decodable>(_ query: Query, source: Source = .default, type: T.Type) async throws -> [T]? {
+    public func get<T: Decodable & Sendable>(_ query: Query, source: Source = .default, type: T.Type) async throws -> [T]? {
         try await get(query, source: source, type: type)
     }
 
-    public func get<T: Decodable>(_ reference: DocumentReference, source: Source = .default, type: T.Type) async throws -> T? {
+    public func get<T: Decodable & Sendable>(_ reference: DocumentReference, source: Source = .default, type: T.Type) async throws -> T? {
         try await get(reference, source: source, type: type)
     }
 
@@ -76,7 +76,7 @@ extension Firestore {
 
 extension Firestore {
 
-    public func set<T: Encodable>(_ data: T, reference: DocumentReference) async throws {
+    public func set<T: Encodable & Sendable>(_ data: T, reference: DocumentReference) async throws {
         try await set(data, merge: true, reference: reference)
     }
 }
